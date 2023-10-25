a1fb7bf0fb963a30629c583434374ff081610b83
Mark Khramov <hramov.mark321@gmail.com>
2023-10-26 02:21:05 +0300

    update

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import {Layout as AntLayout} from "antd";
 import s from './Layout.module.css'
+import Header from "../header/Header";
 
 
 type Props = {
@@ -8,6 +9,7 @@ type Props = {
 const Layout = ({children}: Props) => {
     return (
         <div className={s.main}>
+            <Header/>
             <AntLayout.Content style={{height: '100%'}}>
             { children }
             </AntLayout.Content>
--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from "../../components/layout/Layout";
 
 const Login = () => {
     return (
         <div>
+            <Layout>
             <h1>Login</h1>
+            </Layout>
         </div>
     );
 };
