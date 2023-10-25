852fba1c06663fdca92568b8ff3bff69506b8ade
Mark Khramov <hramov.mark321@gmail.com>
2023-10-26 02:01:12 +0300

    fix

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 import './index.css';
+import {Paths} from "./path";
+
+const router = createBrowserRouter([
+    {
+        path: Paths.home,
+        element: <h1>Employees</h1>
+    },
+    {
+        path: Paths.login,
+        element: <Login/>
+    },
+    {
+        path: Paths.register,
+        element: <Register/>
+    },
+])
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <h1>Hello World</h1>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router}/>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
